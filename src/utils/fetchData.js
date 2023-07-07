d745c9e26ac082080bdc88048087a49bccca8df9
Om Nigam <Friday@Oms-MacBook-Air.local>
2023-07-08 03:02:42 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,26 @@
+ 
+  
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key':'c7fb59274fmshcc7d0d24b75ad1fp11a7dbjsncf0de1cc83eb',
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': 'c7fb59274fmshcc7d0d24b75ad1fp11a7dbjsncf0de1cc83eb',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
+
+ 
